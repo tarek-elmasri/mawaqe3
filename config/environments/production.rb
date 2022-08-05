4440d26c98aba9c3b9827bde8cb539a3ab10e2e3
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-08-05 22:46:56 +0300

    disabling trurbo links preload issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # preload javascript_pack_tags messages
+  config.action_view.preload_links_header = false
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
